95a56a87e8ea1e8ec0cc4757a6021395f70628f9
Noah Laux <noahlaux@gmail.com>
2014-08-19 11:07:13 +0200

    Made it nifty

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ document.addEventListener('polymer-ready', function(e) {
     var trackList = document.querySelector('#trackList');
     var searchInput = document.querySelector('#searchString');
 
-    searchInput.addEventListener('keypress', function(e) {
+    searchInput.addEventListener('keydown', function(e) {
         trackList.search = e.currentTarget.value;
     });
 });
