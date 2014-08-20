f80e0c583473735669b16b9383366eba6d915774
Noah Laux <noahlaux@gmail.com>
2014-08-20 14:27:37 +0200

    added stuff

--- app/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('polymer-ready', function(e) {
-    var trackList = document.querySelector('#trackList');
-    var searchInput = document.querySelector('#searchString');
-
-    searchInput.addEventListener('keydown', function(e) {
-        trackList.search = e.currentTarget.value;
-    });
-});
